cbd8964fe03d757e2705ec05a57afd9a8633902d
Shantanu <moreshantanu09@gmail.com>
2024-06-17 18:40:01 +0530

    multer and cloudinary setups

---
+++ src/models/user.models.js
@@ -0,0 +1,92 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        index: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        index: true,
+        trim: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password:  {
+        type: String,
+        required: [true, "Password is must here!!"]
+    },
+    refreshToken: {
+        type: String
+    }
+
+}, {timestamps: true})
+
+/*
+
+//here we are not using arrow function because it does not contain this(current reference),
+ but normal function do. 
+and it takes time so we are writing async 
+and as we use middleware we give next as flag*/
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+        _id: this._id,
+        userName: this.userName,
+        email: this.email,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.genarateRefreshToken = function(){
+    jwt.sign(
+        {
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+export const User = mongoose.model("User", userSchema)
---
+++ src/utils/cloudinary.service.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+const uploadOnCloudinary = async (filePath) =>{
+    try {
+        if(!filePath){
+            return null
+        }
+
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto"
+        })
+
+        //successfully uploaded file on cloudinary
+        console.log("File uploaded successfully !!", response.url);
+        return response
+    } catch (error) {
+        //This will remove file from local serer as upload is failed
+        fs.unlinkSync(filePath)
+        return null
+    }
+}
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+export {uploadOnCloudinary}
